81063d06868391745501d36593ac57dbb33f56d4
DakshaT <76222923+DakshaT@users.noreply.github.com>
2023-07-14 15:22:11 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,25 @@
+package com.blinkx;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req , HttpServletResponse res) throws ServletException, IOException{
+		PrintWriter out = res.getWriter();
+		System.out.println("Hi");
+		out.write("Hi");
+		ServletContext ctx = getServletContext();
+		String str = ctx.getInitParameter("name");
+		System.out.println(str);
+		out.write(str);
+		
+	}
+
+}
